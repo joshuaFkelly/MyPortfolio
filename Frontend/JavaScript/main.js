13ed1f5da41583798833274724f7f9d2f8e3a579
Joshua Kelly <jdkpersonal2015@gmail.com>
2022-09-06 16:56:21 -0400

    cleaning up code. too much junk from being confused

---
+++ Frontend/JavaScript/main.js
@@ -0,0 +1,12 @@
+// ScrollReveal().reveal({
+//     reset: true,
+//     distance: "2px",
+//     duration: 25000,
+//     delay: 400
+// })
+
+ScrollReveal().reveal(".projectOne", { delay: 500 })
+ScrollReveal().reveal(".projectTwo", { delay: 500 })
+ScrollReveal().reveal(".projectThree", { delay: 500 })
+ScrollReveal().reveal(".projectFour", { delay: 100 })
+ScrollReveal().reveal(".projectFive", { delay: 100 })
