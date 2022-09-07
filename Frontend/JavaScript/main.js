f400a15a5a553f045cf362fbef0637b7e22012be
Joshua Kelly <jdkpersonal2015@gmail.com>
2022-09-06 20:11:55 -0400

    working on animations now. really enjoying this process

--- Frontend/JavaScript/main.js
+++ Frontend/JavaScript/main.js
@@ -1,12 +1,5 @@
-ScrollReveal().reveal({
-    reset: true,
-    distance: "2px",
-    duration: 25000,
-    delay: 400
-})
-
-ScrollReveal().reveal("#projectOne", { delay: 1000 })
-ScrollReveal().reveal("#projectTwo", { delay: 1000 })
-ScrollReveal().reveal("#projectThree", { delay: 1000 })
-ScrollReveal().reveal("#projectFour", { delay: 1000 })
-ScrollReveal().reveal("#projectFive", { delay: 1000 })
+ScrollReveal().reveal("#projectOne", { reset: true, delay: 1000, easing: "ease-in-out" })
+ScrollReveal().reveal("#projectTwo", { reset: true, delay: 1000, easing: "ease-in-out" })
+ScrollReveal().reveal("#projectThree", { reset: true, delay: 1000, easing: "ease-in-out" })
+ScrollReveal().reveal("#projectFour", { reset: true, delay: 1000, easing: "ease-in-out" })
+ScrollReveal().reveal("#projectFive", { reset: true, delay: 1000, easing: "ease-in-out" })
