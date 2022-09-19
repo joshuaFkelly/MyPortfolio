709a30825925583e866d4e772dd2622f5e92d0b7
Joshua Kelly <jdkpersonal2015@gmail.com>
2022-09-19 17:17:42 -0400

    changed background, moved navbar, added skills, added scrollTo animation. i still hate this

--- Frontend/JavaScript/main.js
+++ Frontend/JavaScript/main.js
@@ -3,3 +3,26 @@ ScrollReveal().reveal("#projectTwo", { reset: true, delay: 1000, easing: "ease-i
 ScrollReveal().reveal("#projectThree", { reset: true, delay: 1000, easing: "ease-in-out" })
 ScrollReveal().reveal("#projectFour", { reset: true, delay: 1000, easing: "ease-in-out" })
 ScrollReveal().reveal("#projectFive", { reset: true, delay: 1000, easing: "ease-in-out" })
+
+
+
+const skillsNavBtn = document.getElementById("skillsBtn");
+skillsNavBtn.addEventListener("click", (e) => {
+    const skillSection = document.getElementById("skillsSection");
+
+    skillSection.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" })
+})
+
+const projectsNavBtn = document.getElementById("projectsBtn");
+projectsNavBtn.addEventListener("click", (e) => {
+    const projectSection = document.getElementById("projectSection");
+
+    projectSection.scrollIntoView({ behavior: "smooth", block: "start", inline: "start" })
+})
+
+const contactNavBtn = document.getElementById("contactBtn");
+contactNavBtn.addEventListener("click", (e) => {
+    const contactSection = document.getElementById("contactSection");
+
+    contactSection.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" })
+})
